7468a307f1e44b701c8ae74a0c19a56f40154d7c
Rafael Conrado <106274045+RafaelCGConrado@users.noreply.github.com>
2022-11-06 17:42:47 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -9,7 +9,7 @@ struct alunos{
     int *notas;
     double media;
     int desempate;
-    int maior_folha;
+    int index;
 };
 
 //Recebe os dados dos alunos e armazena nas structs
@@ -19,6 +19,9 @@ void leitura(ALUNO **alunos, int n_alunos, int n_notas);
 //Calcula a média de cada aluno
 void calcula_media(ALUNO **alunos, int n_alunos, int n_notas);
 
+//Calcula a maior media da turma
+double maior_media(ALUNO **alunos, int n_alunos);
+
 //Printa os nomes, notas e média de cada aluno.
 //Usada para testes ao longo do código.
 void printa(ALUNO **alunos, int n_alunos, int n_notas);
@@ -26,15 +29,22 @@ void printa(ALUNO **alunos, int n_alunos, int n_notas);
 //Encontra o K utilizado na criação da árvore
 int encontra_k(int n_alunos);
 
-//Efetua troca de posição entre dois alunos
-void swap(ALUNO *aluno1, ALUNO *aluno2);
-
-
 //Faz as comparações, desempata e retorna o "melhor" aluno
 int compara_alunos(ALUNO *aluno1, ALUNO *aluno2, int n_notas);
 
+//Efetua as trocas de posição
+void troca(ALUNO *a1, ALUNO *a2);
+
 //Constrói a árvore utilizada na ordenação
-void constroi_arvore(ALUNO **alunos, ALUNO **arvore, int tamanho_arvore, int n_alunos, int n_notas, int posicao);
+void constroi_arvore(ALUNO **alunos, ALUNO **arvore, int tamanho_arvore, int n_alunos, int n_notas);
+
+//Maximiza o heap que será utilizado na ordenação
+void heapify(ALUNO **arvore, ALUNO **alunos, int tamanho_arvore, int n_alunos, int n_notas, int posicao);
+
 
 //Algoritmo de ordenação
 void torneio_sort(ALUNO **arvore, ALUNO **alunos, int tamanho_arvore, int n_alunos, int n_notas);
+
+
+//Printa seguindo a formatação pedida no final do programa
+void printa_final(ALUNO **alunos, int n_alunos, double maior_media);
